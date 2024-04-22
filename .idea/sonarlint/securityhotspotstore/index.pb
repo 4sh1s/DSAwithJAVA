3accf8517a8442b28069ef023965c692bf98a4d4
4sh1s <tripathy.ashish251997@gmail.com>
2024-04-23 00:27:51 +0530

    Even more methods added to the Linked List Data Structure

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -9,3 +9,11 @@ C
 src/LinkedList.java,0\0\00fd33ac798e066bff270304abba2030492c06ee
 R
 "src/DataStructures/LinkedList.java,e\4\e42c3db0cc21d26174a7d6e48ccf494b883098df
+R
+"src/dataStructures/LinkedList.java,d\d\ddc9d09d5915ee30b4c6c7aafc7a3e7889bd894d
+]
+-src/dataStructures/LinkedList/LinkedList.java,e\5\e5d42586c5cbbac37da847461bce7c1a5d6249d0
+E
+src/running/Main.java,d\6\d616971f1a878573cb53b38425dc434c9f460478
+W
+'src/dataStructures/LinkedList/Main.java,8\6\86f3c76eece416c50cfb49cb020b2a0a89b75186
--- src/dataStructures/LinkedList/Main.java
+++ src/dataStructures/LinkedList/Main.java
@@ -1,6 +1,4 @@
-package running;
-
-import dataStructures.LinkedList.LinkedList;
+package dataStructures.LinkedList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -15,14 +13,31 @@ public class Main {
 //
 //        newLL.printList();
         newLL.append(7);
+        newLL.prepend(89);
+        newLL.getHead();
+        newLL.prepend(976);
+        newLL.getHead();
+
+        System.out.println(newLL.get(0).value+ "\n");
+        System.out.println(newLL.set(2,9989));
+        System.out.println(newLL.set(-2,9989));
+        System.out.println(newLL.set(1,9989));
+        newLL.printList();
+        newLL.reverse();
+        newLL.printList();
+
+
+
 //        int i=9;
 //        while(i>1){
 //            newLL.append(i*i);
 //            i--;
 //        }
-        System.out.println(newLL.removeLast().value);
-        System.out.println(newLL.removeLast().value);
-        System.out.println(newLL.removeLast());
+//        System.out.println(newLL.removeLast().value);
+//        System.out.println(newLL.removeLast().value);
+//        System.out.println(newLL.removeFirst().value);
+//        System.out.println(newLL.removeFirst().value);
+//        System.out.println(newLL.removeLast());
 
 
     }
